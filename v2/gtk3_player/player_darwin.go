5abfcdeb80373adeb43c4075ee8ac6f33f972215
Adrian-George Bostan <adrg@epistack.com>
2022-02-07 13:29:07 +0200

    Update GTK 3 example macOS window handling

--- v2/gtk3_player/player_darwin.go
+++ v2/gtk3_player/player_darwin.go
@@ -1,14 +1,12 @@
 package main
 
 /*
+#cgo CFLAGS: -x objective-c
 #cgo pkg-config: gdk-3.0
+#include <AppKit/AppKit.h>
 #include <gdk/gdk.h>
 
-GDK_AVAILABLE_IN_ALL NSWindow* gdk_quartz_window_get_nswindow(GdkWindow *window);
-
-void* getNsWindow(GdkWindow *w) {
-	return (void*)gdk_quartz_window_get_nswindow(w);
-}
+GDK_AVAILABLE_IN_ALL NSView* gdk_quartz_window_get_nsview(GdkWindow *window);
 */
 import "C"
 import (
@@ -18,13 +16,7 @@ import (
 	"github.com/gotk3/gotk3/gdk"
 )
 
-type NSWindow struct {
-	ID unsafe.Pointer
-}
-
 func setPlayerWindow(player *vlc.Player, window *gdk.Window) error {
-	handle := &NSWindow{
-		unsafe.Pointer(C.getNsWindow((*C.GdkWindow)(unsafe.Pointer(window.Native())))),
-	}
-	return player.SetNSObject(uintptr(unsafe.Pointer(handle)))
+	handle := unsafe.Pointer(C.gdk_quartz_window_get_nsview((*C.GdkWindow)(unsafe.Pointer(window.GObject))))
+	return player.SetNSObject(uintptr(handle))
 }
